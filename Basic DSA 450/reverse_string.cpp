9de607198e9c4a69b1b4703b3a815ed87ba67728
Ahnaf Tahmid <86840261+AhnafTahmid1776@users.noreply.github.com>
2022-12-02 19:51:52 +0600

    Create reverse_string.cpp

---
+++ Basic DSA 450/reverse_string.cpp
@@ -0,0 +1,17 @@
+    #include <iostream>
+    using namespace std;
+    int main ()
+    {
+        int t;
+        cin >> t;
+        while(t--){
+            string s;
+            cin >> s;
+            int n=s.length();
+            for(int i=0; i<n/2;i++){
+                    swap(s[i],s[n-i-1]);
+          }
+          cout<< s<<endl;
+        }
+    }
+
