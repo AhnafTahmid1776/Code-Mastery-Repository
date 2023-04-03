040e314e481d982f53e23313ef63f95ecfed28a2
Ahnaf Tahmid <86840261+AhnafTahmid1776@users.noreply.github.com>
2023-04-03 06:22:01 +0600

    Update binarySearch.cpp

--- Basic DSA 450/binarySearch.cpp
+++ Basic DSA 450/binarySearch.cpp
@@ -7,7 +7,7 @@ int binarySearch(int arr[], int size, int key){
    int start= 0;
    int end =size-1;
 
-   int mid=(start+end)/2;
+   int mid=start+(end-start)/2;
 
    while(start<=end){
     if(arr[mid] == key){
@@ -20,7 +20,7 @@ int binarySearch(int arr[], int size, int key){
     else{
         start =mid +1;
     }
-    mid= (start+end)/2;
+   start+(end-start)/2
    }
    return -1;
 
