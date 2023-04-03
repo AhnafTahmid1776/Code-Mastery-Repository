b1c0289d81bba5006d39721653572b1347830fdb
Ahnaf Tahmid <86840261+AhnafTahmid1776@users.noreply.github.com>
2023-04-03 06:16:34 +0600

    Create binarySearch.cpp

---
+++ Basic DSA 450/binarySearch.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h> //header
+
+using namespace std;
+
+
+int binarySearch(int arr[], int size, int key){
+   int start= 0;
+   int end =size-1;
+
+   int mid=(start+end)/2;
+
+   while(start<=end){
+    if(arr[mid] == key){
+        return mid;
+    }
+    if(arr[mid]>key){
+        end =mid-1;
+
+    }
+    else{
+        start =mid +1;
+    }
+    mid= (start+end)/2;
+   }
+   return -1;
+
+}
+
+
+
+int main()
+{
+ int odd[5]={1,2,32,44,55};
+ int even[6]={3,5,7,8,10,12};
+
+int binodd=binarySearch(odd,5,44);
+
+cout << binodd<< endl;
+}
+
